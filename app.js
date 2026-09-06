ea17a13c4a69aba41b88cd4c4164f2c46d062bd1
mediacaopedagogica <keisepamela.mediadorapedagogica@gmail.com>
2026-09-06 10:51:23 -0400

    Publish real-time online status

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 import { CONFIG } from './config.js'
+import './realtime-presence.js'
 import './study.js'
 import './study-document.js'
 import './retention-notice.js'
