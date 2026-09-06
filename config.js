7aa821bcc08db270c90d0abfbea2e1e717d9fd9b
mediacaopedagogica <keisepamela.mediadorapedagogica@gmail.com>
2026-09-06 10:26:13 -0400

    Atualiza nome do aplicativo para Cantinho da Isa

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ export const CONFIG = {
   SUPABASE_URL: 'https://srazofmrxzwqcavnhplq.supabase.co',
   SUPABASE_KEY: 'sb_publishable_DkrgCnVQ6Fn2jBahb2fCPA_VOwWWAcr',
   VAPID_PUBLIC_KEY: 'BHitKQYrrLqSBcX5cATGKWgow1iN7kstrctNKfwkqBZRIx5tt-UNpiwmCNwjhEnTkWORRXm3M_93_-WomAoxiyk',
-  APP_NAME: 'Nosso Cantinho'
+  APP_NAME: 'Cantinho da Isa'
 }
