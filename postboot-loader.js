4518ba15beafc53ff8964e37de9d47c6f72b10bd
mediacaopedagogica <keisepamela.mediadorapedagogica@gmail.com>
2026-09-07 16:11:37 -0400

    Refresh audio player modules

--- postboot-loader.js
+++ postboot-loader.js
@@ -15,11 +15,11 @@ const paths=dedicatedMobile?[
   './call-manager.js?v=7-mobile-fast',
   './mobile-native.js?v=3-no-blank',
   './notifications-v2.js?v=9-stable',
-  './extras-loader.js?v=17-audio-file-mime'
+  './extras-loader.js?v=18-audio-player'
 ]:[
   './mobile-responsive-v2.js?v=15-android-touch',
   './notifications-v2.js?v=9-stable',
-  './extras-loader.js?v=15-audio-file-mime'
+  './extras-loader.js?v=16-audio-player'
 ]
 const results=await Promise.allSettled(paths.map(loadWithRetry))
 window.__ISA_EXTRAS_READY__=true
