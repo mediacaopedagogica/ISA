6b9801486b1eb4f88504f81d315ac0c2abe4ee70
mediacaopedagogica <keisepamela.mediadorapedagogica@gmail.com>
2026-09-07 19:18:07 -0400

    Load Cobrinha Pastel menu extension for Isa

--- extras-loader.js
+++ extras-loader.js
@@ -30,6 +30,7 @@ async function loadCoreExtras(){
   if(isIsa()){
     jobs.push(loadOnce('isa-tools','./isa-tools.js?v=9-study-fix'))
     jobs.push(loadOnce('games','./games-menu.js?v=3-mobile-games'))
+    jobs.push(loadOnce('snake-game','./games-menu-snake.js?v=1'))
     if(!dedicatedMobile)jobs.push(loadOnce('games-notebook-fit','./games-notebook-fit.js?v=1'))
   }
   const result=await Promise.allSettled(jobs)
