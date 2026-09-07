f796e2c60d7b8e86281aaab90fa2b0335db2dcb4
mediacaopedagogica <keisepamela.mediadorapedagogica@gmail.com>
2026-09-07 14:07:59 -0400

    Load fixed mobile back navigation

--- postboot-loader.js
+++ postboot-loader.js
@@ -12,7 +12,7 @@ async function loadWithRetry(path){
 
 const dedicatedMobile=new URLSearchParams(location.search).get('mobile')==='1'
 const paths=dedicatedMobile?[
-  './mobile-native.js?v=1',
+  './mobile-native.js?v=2-back-fix',
   './notifications-v2.js?v=9-stable',
   './extras-loader.js?v=14-mobile-native'
 ]:[
