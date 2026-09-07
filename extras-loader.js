2b28c4e0ad41be9c408a02165ebf3e3219ba171d
mediacaopedagogica <keisepamela.mediadorapedagogica@gmail.com>
2026-09-07 16:03:25 -0400

    Refresh audio MIME module on desktop and mobile

--- extras-loader.js
+++ extras-loader.js
@@ -23,7 +23,7 @@ async function loadCoreExtras(){
     loadOnce('groups','./group-controls.js?v=5-stable'),
     loadOnce('pins','./conversation-pins.js?v=5-direct-pin'),
     loadOnce('paused-friends','./paused-friends-filter.js?v=3-stable'),
-    loadOnce('family-media','./family-media-menu-v2.js?v=6-audio-mime'),
+    loadOnce('family-media','./family-media-menu-v2.js?v=7-audio-file-mime'),
     loadOnce('links','./link-preview.js?v=5-stable')
   ]
   if(!dedicatedMobile)jobs.push(loadOnce('calls','./call-manager.js?v=4-stable'))
