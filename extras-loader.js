ff0a3ecb5d4036f0887641ff9181bf078275bd9c
mediacaopedagogica <keisepamela.mediadorapedagogica@gmail.com>
2026-09-07 20:02:08 -0400

    Refresh construction farm loader

--- extras-loader.js
+++ extras-loader.js
@@ -31,7 +31,7 @@ async function loadCoreExtras(){
     jobs.push(loadOnce('isa-tools','./isa-tools.js?v=9-study-fix'))
     jobs.push(loadOnce('games','./games-menu.js?v=3-mobile-games'))
     jobs.push(loadOnce('snake-game','./games-menu-snake.js?v=1'))
-    jobs.push(loadOnce('farm-game','./games-menu-farm.js?v=2-realism-identity'))
+    jobs.push(loadOnce('farm-game','./games-menu-farm.js?v=3-construction-interaction'))
     if(!dedicatedMobile)jobs.push(loadOnce('games-notebook-fit','./games-notebook-fit.js?v=1'))
   }
   const result=await Promise.allSettled(jobs)
