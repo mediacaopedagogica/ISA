4f3e03421c931b8cf7ec350e942009efd44b0cb6
mediacaopedagogica <keisepamela.mediadorapedagogica@gmail.com>
2026-09-09 19:07:34 -0400

    Carrega ajustes v5 da Nossa Rede com CSS

---
+++ nossa-rede-policy-v5-loader.js
@@ -0,0 +1,4 @@
+// Loader leve da Nossa Rede v5.
+if(!document.getElementById('nossaRedePolicyV5Css')){const l=document.createElement('link');l.id='nossaRedePolicyV5Css';l.rel='stylesheet';l.href='./nossa-rede-policy-v5.css?v=1-timeline-social';document.head.appendChild(l)}
+await import('./nossa-rede-policy-v5.js?v=1-timeline-social');
+window.__ISA_NOSSA_REDE_V5__?.patch?.();
