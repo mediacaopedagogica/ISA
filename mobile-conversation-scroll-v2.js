b57ec880ca11e3d0722886c4c7722d761df08668
mediacaopedagogica <keisepamela.mediadorapedagogica@gmail.com>
2026-09-11 10:38:24 -0400

    feat: load next-birthday card helper without touching existing UI

--- mobile-conversation-scroll-v2.js
+++ mobile-conversation-scroll-v2.js
@@ -9,3 +9,4 @@
   }
 })();
 import('./nossa-rede-profile-search-v1.js?v=2-family-profile-search').then(()=>window.__ISA_NOSSA_REDE_PROFILE_SEARCH__?.scan?.()).catch(()=>{});
+import('./nossa-rede-birthday-card-v2.js?v=1-next-15-days').then(()=>window.__ISA_BIRTHDAY_CARD_V2__?.scan?.()).catch(()=>{});
