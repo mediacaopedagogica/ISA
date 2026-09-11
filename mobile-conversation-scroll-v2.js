5be9133ec1fc75717f0eabaec8a720fae112e378
mediacaopedagogica <keisepamela.mediadorapedagogica@gmail.com>
2026-09-11 10:28:12 -0400

    chore: refresh mobile scroll and family search cache keys

--- mobile-conversation-scroll-v2.js
+++ mobile-conversation-scroll-v2.js
@@ -4,8 +4,8 @@
     const l=document.createElement('link')
     l.id='isaMobileConversationScrollV2'
     l.rel='stylesheet'
-    l.href='./mobile-conversation-scroll-v2.css?v=2-vertical-all-mobile'
+    l.href='./mobile-conversation-scroll-v2.css?v=3-vertical-all-mobile'
     document.head.appendChild(l)
   }
 })();
-import('./nossa-rede-profile-search-v1.js?v=1-family-profile-search').then(()=>window.__ISA_NOSSA_REDE_PROFILE_SEARCH__?.scan?.()).catch(()=>{});
+import('./nossa-rede-profile-search-v1.js?v=2-family-profile-search').then(()=>window.__ISA_NOSSA_REDE_PROFILE_SEARCH__?.scan?.()).catch(()=>{});
