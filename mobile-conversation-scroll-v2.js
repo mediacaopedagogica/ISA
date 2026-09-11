109a66ffc5bc3eca450fad89103a6e3466195ce0
mediacaopedagogica <keisepamela.mediadorapedagogica@gmail.com>
2026-09-11 10:24:08 -0400

    fix: bump mobile conversation scroll stylesheet

--- mobile-conversation-scroll-v2.js
+++ mobile-conversation-scroll-v2.js
@@ -4,6 +4,6 @@
   const l=document.createElement('link')
   l.id='isaMobileConversationScrollV2'
   l.rel='stylesheet'
-  l.href='./mobile-conversation-scroll-v2.css?v=1-visible-scroll'
+  l.href='./mobile-conversation-scroll-v2.css?v=2-vertical-all-mobile'
   document.head.appendChild(l)
 })();
